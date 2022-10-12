fcc9df5a84f59609e135d079adf5b5bc12a6f02d
Andre Silva <guerreirografico@gmail.com>
2022-10-11 22:08:05 -0300

    create states

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import { QuizContext } from "./context/quiz";
+
 import { Welcome } from "./components/Welcome/Welcome";
+import { Question } from "./components/Question/Question";
+
 import "./App.css";
 
 export const App = () => {
+  const [quizState, dispatch]= useContext(QuizContext);
+
   return (
    <div className='App'>
-    <h1>Quiz de Programação</h1>
-    <Welcome />
+    <h1>Quiz Biblico</h1>
+    {quizState.gameStage === "Start" && <Welcome />}
+    {quizState.gameStage === "Playing" && <Question />}
    </div>
   )
 }
