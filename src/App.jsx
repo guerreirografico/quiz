0d320a9c8f8c50c3570af2861cccbac1e9ff6fef
Andre Silva <guerreirografico@gmail.com>
2022-10-12 14:08:13 -0300

    create component gameOver

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { QuizContext } from "./context/quiz";
 
 import { Welcome } from "./components/Welcome/Welcome";
 import { Question } from "./components/Question/Question";
+import { GameOver} from "./components/GameOver/GameOver";
 
 import "./App.css";
-import { useEffect } from "react";
 
 export const App = () => {
   const [quizState, dispatch]= useContext(QuizContext);
 
   useEffect(() => {
     dispatch({ type: "REORDER_QUESTIONS" })
-  }, [])
+  }, []);
 
   return (
    <div className='App'>
     <h1>Quiz Biblico</h1>
     {quizState.gameStage === "Start" && <Welcome />}
     {quizState.gameStage === "Playing" && <Question />}
+    {quizState.gameStage === "End" && <GameOver />}
    </div>
   )
 }
